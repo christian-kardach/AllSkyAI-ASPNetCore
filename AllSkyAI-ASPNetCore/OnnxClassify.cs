f6a344e0866b5a3cdf4f3c65262dc0f69474d5bd
Christian Kardach <christian.kardach@gmail.com>
2023-12-05 22:08:38 +0100

    Update OnnxClassify.cs

--- AllSkyAI-ASPNetCore/OnnxClassify.cs
+++ AllSkyAI-ASPNetCore/OnnxClassify.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.ML.OnnxRuntime.Tensors;
 using Microsoft.ML.OnnxRuntime;
 using System.Net;
@@ -10,6 +10,7 @@ using AllSkyAI_ASPNetCore.Controllers;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 using System.Globalization;
+using System;
 
 namespace AllSkyAI_ASPNetCore
 {
@@ -30,10 +31,21 @@ namespace AllSkyAI_ASPNetCore
 
         CultureInfo cultureInfo = CultureInfo.GetCultureInfo("en-US");
 
+        string[] Labels;
+
         public OnnxClassify()
         {
             _logger = LoggerFactory.Create(options => { }).CreateLogger<OnnxClassify>();
             string modelFilePath = ".\\model\\" + configuration.Model;
+            if (!File.Exists(modelFilePath))
+            {
+                Console.WriteLine($"Model file does not exists: {modelFilePath}");
+                return;
+            }
+
+            string labelsFilePath = ".\\model\\" + configuration.Labels;
+            Labels = LabelMap.readLabels(labelsFilePath);
+            
             session = new InferenceSession(modelFilePath);
         }
 
@@ -71,25 +83,75 @@ namespace AllSkyAI_ASPNetCore
             var dimentions = metaData.Dimensions.ToList();
             int height = dimentions[1];
             int width = dimentions[2];
+            int channels = dimentions[3];
 
             // Read image
             Image<Rgb24> image = Image.Load<Rgb24>(saveLocation);
-            image.Mutate(x => x.Resize(width, height, KnownResamplers.Lanczos3));
-            image.Mutate(x => x.Grayscale());
+            
+            if(channels == 1)
+            {
+                image.Mutate(x => x.Grayscale());
+            }
+            
+            if(height == width) // We need to reize to height and crop
+            {
+                image.Mutate(x =>
+                    x.Resize(
+                        new ResizeOptions()
+                        {
+                            Mode = ResizeMode.Max,
+                            Size = new Size(int.MaxValue, height)
+                        }
+                    )
+                );
+
+                var size = image.Size();
+                var l = (size.Width / 2)-256;
+                var t = 0;
+                var r = (size.Width / 2) + 256;
+                var b = size.Height;
+
+                image.Mutate(x => x.Crop(Rectangle.FromLTRB(l, t, r, b)));
+            }
+            // Resize to fix input dimentions
+            else
+            {
+                image.Mutate(x => x.Resize(width, height, KnownResamplers.Lanczos3));
+            }
 
             // Preprocess image
-            Tensor<float> input = new DenseTensor<float>(new[] { 1, height, width, 1 });
-            image.ProcessPixelRows(accessor =>
+            Tensor<float> input = new DenseTensor<float>(new[] { 1, height, width, channels });
+            if(channels == 1)
             {
-                for (int y = 0; y < accessor.Height; y++)
+                image.ProcessPixelRows(accessor =>
                 {
-                    Span<Rgb24> pixelSpan = accessor.GetRowSpan(y);
-                    for (int x = 0; x < accessor.Width; x++)
+                    for (int y = 0; y < accessor.Height; y++)
                     {
-                        input[0, y, x, 0] = ((pixelSpan[x].R));
+                        Span<Rgb24> pixelSpan = accessor.GetRowSpan(y);
+                        for (int x = 0; x < accessor.Width; x++)
+                        {
+                            input[0, y, x, 0] = ((pixelSpan[x].R));
+                        }
                     }
-                }
-            });
+                });
+            }
+            else
+            {
+                image.ProcessPixelRows(accessor =>
+                {
+                    for (int y = 0; y < accessor.Height; y++)
+                    {
+                        Span<Rgb24> pixelSpan = accessor.GetRowSpan(y);
+                        for (int x = 0; x < accessor.Width; x++)
+                        {
+                            input[0, y, x, 0] = ((pixelSpan[x].R));
+                            input[0, y, x, 1] = ((pixelSpan[x].G));
+                            input[0, y, x, 2] = ((pixelSpan[x].B));
+                        }
+                    }
+                });
+            }
+            
 
             // Setup inputs
             var inputs = new List<NamedOnnxValue>
@@ -106,7 +168,7 @@ namespace AllSkyAI_ASPNetCore
             IEnumerable<float> softmax = output.Select(x => (float)Math.Exp(x) / sum);
 
             // Extract classes
-            IEnumerable<Prediction> classificationScore = softmax.Select((x, i) => new Prediction { Label = LabelMap.Labels[i], Confidence = x })
+            IEnumerable<Prediction> classificationScore = softmax.Select((x, i) => new Prediction { Label = Labels[i], Confidence = x })
                                .OrderByDescending(x => x.Confidence)
                                .Take(1);
             watch.Stop();
