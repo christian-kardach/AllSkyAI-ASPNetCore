e94b09ddbd288c33b116148947e8bca910d67318
Christian Kardach <christian.kardach@gmail.com>
2023-12-05 22:07:15 +0100

    Update LabelMap.cs

--- AllSkyAI-ASPNetCore/LabelMap.cs
+++ AllSkyAI-ASPNetCore/LabelMap.cs
@@ -1,10 +1,23 @@
-﻿namespace AllSkyAI_ASPNetCore
+namespace AllSkyAI_ASPNetCore
 {
     public class LabelMap
     {
         public static readonly string[] Labels = new[] {"clear",
                                                         "heavy_clouds",
                                                         "light_clouds",
+                                                        "not_running",
                                                         "precipitation"};
+
+        public static string[] readLabels(string labelsPath)
+        {
+            List<string> _labels = new List<string>();
+
+            var lines = File.ReadLines(labelsPath);
+            foreach (var line in lines)
+            {
+                _labels.Add(line.Trim());
+            }
+            return _labels.ToArray();
+        }
     }
 }
